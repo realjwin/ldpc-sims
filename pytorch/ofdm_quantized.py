6e2e6fadb9ebf9671352cd56d44b588d0926522f
Jacob Winick <realjwin@gmail.com>
2019-12-04 00:15:00 -0600

    making ofdm quantized have less SNR values

--- pytorch/ofdm_quantized.py
+++ pytorch/ofdm_quantized.py
@@ -11,7 +11,7 @@ from ofdm_functions import gen_data, gen_qdata
 ofdm_size = 32
 num_epochs = 2000
 batch_size = np.power(2, 14) #CHANGE THIS
-lr = .01
+lr = .1
 
 qbits = np.array([1, 3, 5])
 clipdb = np.array([0, 5])
@@ -47,33 +47,36 @@ with open(tx_filepath, 'rb') as f:
 
 for snrdb_idx, snrdb_val in enumerate(snrdb):
     
-    rx_signal, rx_symbols, rx_llrs = gen_data(tx_symbols, snrdb_val, ofdm_size)
-    
-    pretrained = pretrained_filenames[snrdb_idx]
-    
-    for qbits_idx, qbits_val in enumerate(qbits):
-        for clipdb_idx, clipdb_val in enumerate(clipdb):
-            
-            print('Training SNR: {}, Q-Bits: {}, Clip: {} dB'.format(srndb_val, qbits_val, clipdb_val))
-    
-            clip_ratio = np.power(10, (clipdb_val/10))
-            
-            #--- GENERATE QUNATIZED DATA ---#
+    if np.mod(snrdb_val, 2) == 0:    
+        rx_signal, rx_symbols, rx_llrs = gen_data(tx_symbols, snrdb_val, ofdm_size)
         
-            qrx_signal, qrx_symbols, qrx_llrs = gen_qdata(rx_signal, snrdb_val, qbits_val, clip_ratio, ofdm_size)
+        pretrained = pretrained_filenames[snrdb_idx]
+        
+        for qbits_idx, qbits_val in enumerate(qbits):
+            for clipdb_idx, clipdb_val in enumerate(clipdb):
                 
-            input_samples = np.concatenate((qrx_signal.real.T, qrx_signal.imag.T), axis=1)
-            input_samples = input_samples.reshape(-1, 2*ofdm_size)
+                print('Training SNR: {}, Q-Bits: {}, Clip: {} dB'.format(snrdb_val, qbits_val, clipdb_val))
+        
+                clip_ratio = np.power(10, (clipdb_val/10))
+                
+                #--- GENERATE QUNATIZED DATA ---#
             
-            output_samples = rx_llrs.reshape(-1, 2*ofdm_size)
-            
-            #--- TRAIN NETWORK ---#
-            
-            filename = train_nn(input_samples, output_samples, timestamp, snrdb_val, lr, qbits_val, clipdb_val, ofdm_size, num_epochs, batch_size, load_model=pretrained)
-            
-            filenames.append(filename)
+                qrx_signal, qrx_symbols, qrx_llrs = gen_qdata(rx_signal, snrdb_val, qbits_val, clip_ratio, ofdm_size)
+                    
+                input_samples = np.concatenate((qrx_signal.real.T, qrx_signal.imag.T), axis=1)
+                input_samples = input_samples.reshape(-1, 2*ofdm_size)
+                
+                output_samples = rx_llrs.reshape(-1, 2*ofdm_size)
+                
+                #--- TRAIN NETWORK ---#
+                
+                filename = train_nn(input_samples, output_samples, timestamp, snrdb_val, lr, qbits_val, clipdb_val, ofdm_size, num_epochs, batch_size, load_model=pretrained)
+                
+                filenames.append(filename)
 
 #--- SAVE LIST OF FILENAMES ---#
+
+snrdb = np.linspace(0, 10, 6)
 
 modelfile = results + '_quantized.pkl'
 modelpath = 'results/' + modelfile
