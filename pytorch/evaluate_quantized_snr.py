c276898c02b607bd5fc8c6dfe115f1b5e4104695
Jacob Winick <realjwin@gmail.com>
2019-12-18 12:37:12 -0600

    adding some small changes

--- pytorch/evaluate_quantized_snr.py
+++ pytorch/evaluate_quantized_snr.py
@@ -19,7 +19,10 @@ batch_size = 2**10
 num_batches = num_samples // batch_size
 clamp_value = 100
 
-results = '20191217-060437_tx=20191213-234355_unquantized_withsnr'
+qbits = 3
+clip_ratio = np.power(10, 0/10)
+
+results = '20191214-195544_tx=20191213-234355_quantized_withsnr'
 
 #--- LOAD RESULTS ---#
 
@@ -28,11 +31,11 @@ results_path = 'outputs/results/' + results + '.pkl'
 with open(results_path, 'rb') as f:
     data = pickle.load(f)
 
-    filename = data['filename']
+    filenames = data['filenames']
     snrdb_low = data['snrdb_low']
     snrdb_high = data['snrdb_high']
     
-snrdb = np.linspace(snrdb_low, snrdb_high, 1)
+snrdb = np.array([15]) #np.linspace(10, 20, 11)
 
 #--- GENERATE BITS ---#
 
@@ -61,7 +64,7 @@ LLRest.eval()
 LLRest.to(device)
 
 #load weights / oss
-filepath = 'outputs/model/' + filename
+filepath = 'outputs/model/' + filenames[0]
 checkpoint = torch.load(filepath, map_location=device)
 
 LLRest.load_state_dict(checkpoint['model_state_dict'])
@@ -73,27 +76,72 @@ train_loss = checkpoint['loss']
 uncoded_ber = np.zeros(snrdb.shape)
 coded_ber = np.zeros(snrdb.shape)
 coded_bler = np.zeros(snrdb.shape)
+
+uncoded_ber_quantized = np.zeros(snrdb.shape)
+coded_ber_quantized = np.zeros(snrdb.shape)
+coded_bler_quantized = np.zeros(snrdb.shape)
+
 uncoded_ber_nn = np.zeros(snrdb.shape)
 coded_ber_nn = np.zeros(snrdb.shape)
 coded_bler_nn = np.zeros(snrdb.shape)
-wmse = np.zeros(snrdb.shape)
+
+wmse_quantized = np.zeros(snrdb.shape)
+wmse_nn = np.zeros(snrdb.shape)
 
 for snrdb_idx, snrdb_val in enumerate(snrdb):
     
     print(snrdb_val)
     
     #--- GENERATE DATA ---#
-    snr = np.power(10, snrdb_val/10)
-    snr = snr * np.ones((num_samples, 1))
+    snr_single = np.power(10, snrdb_val/10)
+    snr = snr_single * np.ones((num_samples, 1))
     
     rx_signal, rx_symbols, rx_llrs, tx_signal = gen_data(tx_symbols, snrdb_val, ofdm_size)
+    received_symbols = rx_signal.reshape((-1, ofdm_size)).T
 
-    input_samples = np.concatenate((rx_signal.real.T, rx_signal.imag.T), axis=1)
+    #keep clip constant, adjust input signal
+    agc_clip = 10
+
+    #compute sigma_rx per dimension (real/imag)
+    #this is approximate incoming signal amplitude
+    #each column is an OFDM symbol with a different SNR
+    sigma_rx =  .5 * (1 + 1/snr_single)
+
+    #compute clipping per signal (this will be very rough b/c )
+    factor = agc_clip / sigma_rx * clip_ratio
+    
+    rx_signal_scaled = factor * received_symbols
+    rx_signal_scaled = rx_signal_scaled.T.reshape((1,-1))
+    
+    qrx_signal = quantizer(rx_signal_scaled, qbits, agc_clip)
+    
+    qrx_signal_rescaled = qrx_signal.reshape((-1, ofdm_size)).T
+    qrx_signal_rescaled = qrx_signal_rescaled / factor
+    
+    deofdm_qsymbols = np.matmul(DFT(ofdm_size), qrx_signal_rescaled)
+    
+    noise_power = .5 * (1 / snr_single)
+    
+    #this is log(Pr=1 / Pr=0) aka +inf = 1, -inf = -1
+    qllr_bit0 = ( np.power(deofdm_qsymbols.real - 1/np.sqrt(2), 2) - 
+                np.power(deofdm_qsymbols.real + 1/np.sqrt(2), 2) ) / (2*noise_power)
+    qllr_bit1 = ( np.power(deofdm_qsymbols.imag - 1/np.sqrt(2), 2) - 
+                np.power(deofdm_qsymbols.imag + 1/np.sqrt(2), 2) ) / (2*noise_power)
+      
+    qllrs = np.concatenate((qllr_bit0.T.reshape((-1,1)), qllr_bit1.T.reshape((-1,1))), axis=1)
+
+    qrx_llrs = qllrs.reshape((1,-1))
+
+    input_samples = np.concatenate((qrx_signal_rescaled.real.T, qrx_signal_rescaled.imag.T), axis=1)
     input_samples = input_samples.reshape(-1, 2*ofdm_size)
     #appends the SNR to the end of this
     input_samples = np.concatenate((input_samples, snr), axis=1)
  
     output_samples = rx_llrs.reshape(-1, 2*ofdm_size)
+
+    qrx_llrs = qrx_llrs.reshape(-1, 2*ofdm_size)
+    rx_llrs = rx_llrs.reshape(-1, 2*ofdm_size)
+    enc_bits = enc_bits.reshape(-1, 2*ofdm_size)
 
     #--- INFERENCE ---#
     
@@ -110,19 +158,22 @@ for snrdb_idx, snrdb_val in enumerate(snrdb):
     
     #--- LLR WMSE PERFORMANCE ---#
     
-    wmse[snrdb_idx] = np.mean((llr_est - output_samples)**2 / (np.abs(output_samples) + 10e-4))
+    wmse_quantized[snrdb_idx] = np.mean((qrx_llrs - rx_llrs)**2 / (np.abs(rx_llrs) + 10e-4))
+    
+    wmse_nn[snrdb_idx] = np.mean((llr_est - rx_llrs)**2 / (np.abs(rx_llrs) + 10e-4))
     
     #compute number flipped? maybe later...
     
     #--- DECODING PERFORMANCE ---#
     
-    enc_bits = enc_bits.reshape(-1, 2*ofdm_size)
-    
-    cbits = (np.sign(output_samples) + 1) // 2
-    bits = decode_bits(output_samples, H, bp_iterations, batch_size, clamp_value)
+    cbits = (np.sign(rx_llrs) + 1) // 2
+    bits = decode_bits(rx_llrs, H, bp_iterations, batch_size, clamp_value)
     
     cbits_nn = (np.sign(llr_est) + 1) // 2
     bits_nn = decode_bits(llr_est, H, bp_iterations, batch_size, clamp_value)
+    
+    cbits_quantized = (np.sign(qrx_llrs) + 1) // 2
+    bits_quantized = decode_bits(qrx_llrs, H, bp_iterations, batch_size, clamp_value)
     
     uncoded_ber[snrdb_idx] = np.mean(np.abs(cbits - enc_bits))
     coded_ber[snrdb_idx] = np.mean(np.abs(bits[:, 0:32] - enc_bits[:, 0:32]))
@@ -132,6 +183,10 @@ for snrdb_idx, snrdb_val in enumerate(snrdb):
     coded_ber_nn[snrdb_idx] = np.mean(np.abs(bits_nn[:, 0:32] - enc_bits[:, 0:32]))
     coded_bler_nn[snrdb_idx] = np.mean(np.sign(np.sum(np.abs(bits_nn - enc_bits), axis=1)))
     
+    uncoded_ber_quantized[snrdb_idx] = np.mean(np.abs(cbits_quantized - enc_bits))
+    coded_ber_quantized[snrdb_idx] = np.mean(np.abs(bits_quantized[:, 0:32] - enc_bits[:, 0:32]))
+    coded_bler_quantized[snrdb_idx] = np.mean(np.sign(np.sum(np.abs(bits_quantized - enc_bits), axis=1)))
+
 #--- SAVE CODED INFORMATION ---#
     
 ber_path = 'outputs/ber/' + results + '.pkl'
@@ -148,7 +203,12 @@ with open(ber_path, 'wb') as f:
             'coded_ber_nn': coded_ber_nn,
             'coded_bler_nn': coded_bler_nn,
             
-            'wmse': wmse,
+            'uncoded_ber_quantized': uncoded_ber_quantized,
+            'coded_ber_quantized': coded_ber_quantized,
+            'coded_bler_quantized': coded_bler_quantized,
+            
+            'wmse_nn': wmse_nn,
+            'wmse_quantized': wmse_quantized
             }
     
     pickle.dump(save_dict, f)
@@ -159,17 +219,20 @@ if plot:
     fig, axes = plt.subplots(1, 2, figsize=(15,7))
     fig.suptitle('NN Performance on Unquantized Inputs', fontsize=16, y=1.02)
              
-    axes[0].semilogy(snrdb, uncoded_ber, '--*', label='Uncoded Traditional')
-    axes[0].semilogy(snrdb, coded_ber, '--*', label='Coded Traditional')
+    axes[0].semilogy(snrdb, uncoded_ber, label='Uncoded Traditional')
+    axes[0].semilogy(snrdb, coded_ber, label='Coded Traditional')
     axes[0].semilogy(snrdb, uncoded_ber_nn, '--+', label='Uncoded NN')
     axes[0].semilogy(snrdb, coded_ber_nn, '--+', label='Coded NN')
+    axes[0].semilogy(snrdb, uncoded_ber_quantized, '--*', label='Uncoded Quantized')
+    axes[0].semilogy(snrdb, coded_ber_quantized, '--*', label='Coded Quantized')
     axes[0].set_title('BER')
     axes[0].set_xlabel('SNR (dB)')
     axes[0].set_ylabel('BER')
     axes[0].legend()
     
-    axes[1].semilogy(snrdb, coded_bler, '--*', label='Traditional')
+    axes[1].semilogy(snrdb, coded_bler, label='Traditional')
     axes[1].semilogy(snrdb, coded_bler_nn, '--+', label='NN')
+    axes[1].semilogy(snrdb, coded_bler_quantized, '--*', label='Quantized')
     axes[1].set_title('BLER')
     axes[1].set_xlabel('SNR (dB)')
     axes[1].set_ylabel('BLER')
@@ -181,7 +244,8 @@ if plot:
     fig, axes = plt.subplots(1, 2, figsize=(15,7))
     fig.suptitle('NN Performance on Unquantized Inputs', fontsize=16, y=1.02)
              
-    axes[0].plot(snrdb, wmse, '--+', label='NN WMSE')
+    axes[0].plot(snrdb, wmse_nn, '--+', label='NN WMSE')
+    axes[0].plot(snrdb, wmse_quantized, '--*', label='Quantized WMSE')
     axes[0].set_title('WMSE')
     axes[0].set_xlabel('SNR (dB)')
     axes[0].set_ylabel('WMSE')
